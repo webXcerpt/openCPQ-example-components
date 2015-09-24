3dc85b0eb765ab19662a4d5b9fc3a286ee6780f9
Heribert Schütz <hs@webxcerpt.com>
2015-09-24 12:37:44 +0200

    Apply Babel only to current package, not to dependencies. (UNTESTED!)

--- webpack.config.js
+++ webpack.config.js
@@ -20,7 +20,7 @@ var config = {
 	},
 	module: {
 		loaders: [
-			{ test: /\.js$/, loader: "babel?stage=0" },
+			{ test: /\.js$/, exclude: /node_modules/, loader: "babel?stage=0" },
 			{ test: /\.json$/, loader: "json" },
 			{ test: /\.css$/, loader: "style!css" },
 			{ test: /\.less$/, loader: "style!css!less" },
